61e9dd4ae5fc63b5f66d527d1adfdbcf876a8100
Serhat <me.serhaterdem@gmail.com>
2024-01-17 01:44:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.ui
+
+import android.content.Context
+import android.content.res.ColorStateList
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.databinding.basicsample.R
+import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
+import com.example.android.databinding.basicsample.util.getAssociatedColor
+
+/**
+ * Plain old activity with lots of problems to fix.
+ */
+class PlainOldActivity : AppCompatActivity() {
+
+    // Obtain ViewModel from ViewModelProviders
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.plain_activity)
+
+        /**
+         * What is the purpose of this variable?
+         * You'll need it to set those layout variables you declared in the <data> block.
+         * Binding classes are generated automatically by the library.
+         */
+        val binding : PlainActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.plain_activity)
+
+        //val color = getAssociatedColor(viewModel.popularity.value!!, context = applicationContext)
+
+        //binding.progressColor=color
+
+        //init binded variables in xml
+
+        //binding.name = "Serhat"
+       // binding.lastname = "Erdem"
+        binding.lifecycleOwner = this
+        binding.viewmodel=viewModel
+
+
+
+
+        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
+        //updateName()
+        //updateLikes()
+    }
+
+    /**
+     * This method is triggered by the `android:onclick` attribute in the layout. It puts business
+     * logic in the activity, which is not ideal. We should do something about that.
+     */
+   // fun onLike(view: View) {
+    //    viewModel.onLike()
+    //    updateLikes()
+   // }
+
+    /**
+     * So much findViewById! We'll fix that with Data Binding.
+     */
+    //private fun updateName() {
+   //     findViewById<TextView>(R.id.plain_name).text = viewModel.name
+   //     findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
+   // }
+
+}
